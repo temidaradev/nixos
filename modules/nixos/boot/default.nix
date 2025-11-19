b0c03ed52f60bc2674e3d5e56616976ca544a4ec
temidaradev <temidaradev@proton.me>
2025-11-19 20:03:02 +0300

    grub boot settings updated

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -17,20 +17,6 @@
       devices = ["nodev"];
       useOSProber = true;
       extraEntriesBeforeNixOS = false;
-      extraEntries = ''
-        menuentry "Arch Linux" {
-          insmod part_gpt
-          insmod fat
-          search --no-floppy --fs-uuid --set=root BCF1-6F58
-          chainloader /EFI/BOOT/BOOTX64.EFI
-        }
-        menuentry "Reboot" {
-          reboot
-        }
-        menuentry "Poweroff" {
-          halt
-        }
-      '';
     };
   };
 }
