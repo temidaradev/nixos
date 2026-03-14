c1b336e256f732b6686bb7fc72d2456b47731022
temidaradev <temidaradev@proton.me>
2026-03-14 11:05:06 +0300

    shitty power settings gone

--- nixos/system/power.nix
+++ nixos/system/power.nix
@@ -1,17 +1,5 @@
 { ... }:
 
 {
-  boot.kernelParams = [ 
-    "processor.max_cstate=1"
-    "amd_pstate=active"
-  ];
-
-  systemd.targets = {
-    sleep.enable = false;
-    suspend.enable = false;
-    hibernate.enable = false;
-    hybrid-sleep.enable = false;
-  };
-
   services.power-profiles-daemon.enable = true;
 }
