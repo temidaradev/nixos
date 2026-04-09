216f1ea362828dc7460f107bb5a021baf1a0f9c3
temidaradev <temidaradev@proton.me>
2026-04-10 01:02:36 +0300

    font fix

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -14,6 +14,7 @@
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
       ipaexfont
+      nerd-fonts.jetbrains-mono
     ];
 
     fontconfig = {
@@ -32,9 +33,15 @@
       };
 
       defaultFonts = {
-        serif = [ "Noto Serif CJK JP" "Noto Serif" "IPAexMincho" ];
-        sansSerif = [ "Noto Sans CJK JP" "Noto Sans" "IPAexGothic" ];
-        monospace = [ "JetBrainsMono Nerd Font" "Noto Sans Mono CJK JP" ];
+        serif = [ "JetBrainsMono Nerd Font" "Noto Serif CJK JP" "Noto Serif" "IPAexMincho" ];
+        sansSerif = [ "JetBrainsMono Nerd Font" "Noto Sans CJK JP" "Noto Sans" "IPAexGothic" ];
+        monospace = [
+          "JetBrainsMono Nerd Font Mono"
+          "JetBrainsMono Nerd Font"
+          "Noto Sans Mono CJK JP"
+          "Noto Sans CJK JP"
+          "IPAexGothic"
+        ];
         emoji = [ "Noto Color Emoji" ];
       };
     };
